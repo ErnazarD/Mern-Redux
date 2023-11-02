4c9cf44c434778c2950476a09714285da02c9239
ErnazarD <ernazardoronov@gmail.com>
2023-11-02 13:53:26 +0900

    I did this change from personal computer and personal account to remote repo

--- backend/routes/userRoutes.js
+++ backend/routes/userRoutes.js
@@ -1,7 +1,7 @@
 import express from 'express';
 const router = express.Router();
 import { authUser } from '../controllers/userController.js';
-
+// I did this change from personal computer and personal account to remote repo
 router.post('/auth', authUser);
 
 export default router;
