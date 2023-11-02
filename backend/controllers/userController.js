bdfe06834a08f170e56bdb837adcb46e9d61fd4f
eradev001 <ernazardev@quve.kr>
2023-11-02 17:53:47 +0900

    commited from

--- backend/controllers/userController.js
+++ backend/controllers/userController.js
@@ -1,7 +1,57 @@
+import asyncHandler from 'express-async-handler'
+
 // @desc  Auth user/set token 
 // route POST /api/users/auth
 // @access Public
-const authUser = (req, res) => {
-    res.status(200).json({ message: 'Auth User'})
-}
+const authUser =  asyncHandler(async (req, res) => {
+    const users = [
+        {
+            id: 1,
+            name: "Don",
+            position: "Junior",
+            email: "don@gmail.com"
+      
+        },
+        {
+            id: 2,
+            name: "era",
+            email: "era@gmail.com"
+        }
+    ]
+
+
+     res.status(200).json({user:users[0]})
+})
+// export { authUser};
+
+// @desc  Register New User
+// route POST /api/users
+// @access Public
+// const registerUser =  asyncHandler(async (req, res) => {
+//     res.status(200).json({ message: 'Register User'});
+// });
+
+// @desc  Logout User 
+// route POST /api/users/logout
+// @access Private
+// const logoutUser =  asyncHandler(async (req, res) => {
+//     res.status(200).json({ message: 'Logout User'})
+// })
+
+// @desc  Get User prifile
+// route Get /api/users/profile
+// @access Private
+// const getUserProfile =  asyncHandler(async (req, res) => {
+//     res.status(200).json({ message: 'User Profile'})
+// })
+
+// @desc  Update User profile
+// route GET /api/users/profile
+// @access Private
+// const updateUserProfile =  asyncHandler(async (req, res) => {
+//     res.status(200).json({ message: 'Update User Profile'})
+// })
+// export { authUser};
+
+
 export { authUser};
--- backend/server.js
+++ backend/server.js
@@ -3,12 +3,13 @@ import dotenv from 'dotenv';
 dotenv.config();
 const port = process.env.PORT || 6000;
 import userRoutes from './routes/userRoutes.js'
+import { notFound, errorHandler } from './middleware/errorMiddleware.js';
 
 const app = express();
 app.use('/api/users', userRoutes);
 
-
-
 app.get('/', (req, res) => res.send('Server is Ready'));
 
+app.use(notFound);
+app.use(errorHandler);
 app.listen(port, () => console.log(`Server started on port ${port}`));
