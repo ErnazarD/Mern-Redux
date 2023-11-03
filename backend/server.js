5b50bf0da6a844eb6a1b7c1c91c4c519a599117e
ErnazarD <ernazardoronov@gmail.com>
2023-11-03 18:12:44 +0900

    configured nodemon, pushed from personal laptop

--- backend/server.js
+++ backend/server.js
@@ -3,10 +3,10 @@ import dotenv from 'dotenv';
 dotenv.config();
 const port = process.env.PORT || 6000;
 import userRoutes from './routes/userRoutes.js';
-import connectDB from './config/db.js';
+// import connectDB from './config/db.js';
 import { notFound, errorHandler } from './middleware/errorMiddleware.js';
 
-connectDB();
+// connectDB();
 
 const app = express();
 app.use('/api/users', userRoutes);
@@ -19,4 +19,4 @@ app.get('/', (req, res) => res.send('Server is modified'));
 
 app.use(notFound);
 app.use(errorHandler);
-app.listen(port, () => console.log(`Server started on port ${port}`));
+app.listen(port, () => console.log(`Server lounched on port ${port}`));
